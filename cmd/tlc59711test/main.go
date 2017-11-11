9b54720390e2e0d2a0a82770dd26d90b6ad45dfe
Jannis Schaefer <j.schaefer@estwx.de>
2017-11-11 16:07:47 +0100

    working dimmer

--- cmd/tlc59711test/main.go
+++ cmd/tlc59711test/main.go
@@ -1,24 +1,37 @@
 package main
 
 import (
+	log "github.com/Sirupsen/logrus"
 	"github.com/schjan/tlc59711"
 	"time"
-	log "github.com/Sirupsen/logrus"
 )
 
 func main() {
 	dev := tlc59711.NewDevice(1)
 
-	dev.Open(0, 0)
+	err := dev.Open(0, 0)
+	defer dev.Close()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
 	for true {
-		dev.SetBuffer(0, 65535)
-		dev.Flush()
-		log.Info("Hell")
-		time.Sleep(1 * time.Second)
-		dev.SetBuffer(0, 0)
-		dev.Flush()
-		log.Info("Dunkel")
-		time.Sleep(1 * time.Second)
+		bla := 0
+		start := time.Now()
+		log.Info("start")
+		for i := uint16(0); i < 65505; i += 10 {
+			SetAllValue(dev, i)
+			bla++
+		}
+		elapsed := time.Since(start)
+
+		log.Infof("%v steps in %v", bla, elapsed)
 	}
+}
+func SetAllValue(dev *tlc59711.Tlc59711, value uint16) {
+	for i := 0; i < 12; i++ {
+		dev.SetBuffer(i, value)
+	}
+	dev.Flush()
 }
--- device.go
+++ device.go
@@ -1,8 +1,10 @@
 package tlc59711
 
 import (
-	"github.com/fulr/spidev"
 	"fmt"
+	"golang.org/x/exp/io/spi"
+	log "github.com/Sirupsen/logrus"
+	"time"
 )
 
 type Max7219Reg byte
@@ -28,28 +30,66 @@ const (
 const LEDCOUNT = 12
 
 type Tlc59711 struct {
-	count  int
-	buffer []uint16
-	spi    *spidev.SPIDevice
+	count   int
+	buffer  []uint16
+	spi     *spi.Device
+	toWrite chan []byte
+	abort   chan struct{}
 }
 
 func NewDevice(cascaded int) *Tlc59711 {
 	buf := make([]uint16, LEDCOUNT*cascaded)
-	return &Tlc59711{count: cascaded, buffer: buf}
+	dev := &Tlc59711{count: cascaded, buffer: buf, toWrite: make(chan []byte, 1000), abort: make(chan struct{}, 1)}
+
+	go dev.worker()
+
+	return dev
 }
 
 func (d *Tlc59711) Open(spibus int, spidevice int) error {
-	devstr := fmt.Sprintf("/dev/spidev%d.%d", spibus, spidevice)
 
-	spi, err := spidev.NewSPIDevice(devstr)
+	devstr := fmt.Sprintf("/dev/spidev%d.%d", spibus, spidevice)
+	dev, err := spi.Open(&spi.Devfs{
+		Dev:      devstr,
+		Mode:     spi.Mode3,
+		MaxSpeed: 500000,
+	})
+	if err != nil {
+		return err
+	}
+	err = dev.SetBitOrder(spi.MSBFirst)
 	if err != nil {
 		return err
 	}
 
-	d.spi = spi
+	d.spi = dev
 	d.init()
 
 	return nil
+}
+
+func (d *Tlc59711) worker() {
+	for true {
+		select {
+		case <-d.abort:
+			return
+		case buf := <-d.toWrite:
+			d.spi.Tx(buf, nil)
+			time.Sleep(4 * time.Microsecond)
+		}
+	}
+}
+
+func (d *Tlc59711) Close() {
+	d.abort <- struct{}{}
+
+	if d.spi == nil {
+		return
+	}
+
+	err := d.spi.Close()
+
+	log.Warn(err)
 }
 
 func (d *Tlc59711) init() {
@@ -57,16 +97,37 @@ func (d *Tlc59711) init() {
 }
 
 func (d *Tlc59711) sendBufferLine(pos int) error {
+	BCr := uint32(0x7F)
+	BCg := BCr
+	BCb := BCg
+
+	command := uint32(0x25)
+	command <<= 5
+	//OUTTMG = 1, EXTGCK = 0, TMGRST = 1, DSPRPT = 1, BLANK = 0 -> 0x16
+	command |= 0x16
+	command <<= 7
+	command |= BCr
+	command <<= 7
+	command |= BCg
+	command <<= 7
+	command |= BCb
+
 	buf := make([]byte, 28)
 	for i, val := range d.buffer {
-		buf[i*2] = uint8(val >> 8)
-		buf[i*2+1] = uint8(val & 0xFF)
+		buf[i*2+4] = uint8(val >> 8)
+		buf[i*2+5] = uint8(val & 0xFF)
 	}
 
-	_, err := d.spi.Xfer(buf)
-	if err != nil {
-		return err
-	}
+	buf[0] = uint8(command >> 24)
+	buf[1] = uint8(command >> 16)
+	buf[2] = uint8(command >> 8)
+	buf[3] = uint8(command & 0xFF)
+
+	d.toWrite <- buf
+	//err := d.spi.Tx(buf, nil)
+	//if err != nil {
+	//	return err
+	//}
 
 	return nil
 }
@@ -76,12 +137,17 @@ func (d *Tlc59711) SetBuffer(id int, value uint16) {
 }
 
 func (d *Tlc59711) Flush() error {
-	for i := 0; i < LEDCOUNT; i++ {
-		err := d.sendBufferLine(i)
-		if err != nil {
-			return err
-		}
+	err := d.sendBufferLine(0)
+	if err != nil {
+		return err
 	}
+
+	//for i := 0; i < LEDCOUNT; i++ {
+	//	err := d.sendBufferLine(i)
+	//	if err != nil {
+	//		return err
+	//	}
+	//}
 
 	return nil
 }
